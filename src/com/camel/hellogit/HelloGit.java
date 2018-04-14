5bcd0c8a37185a73a825c4663caf5ec8bc9a2615
ngchanfung <ngchanfung@gmail.com>
2018-04-14 23:57:22 +0800

    第一次本地提交

---
+++ src/com/camel/hellogit/HelloGit.java
@@ -0,0 +1,10 @@
+package com.camel.hellogit;
+
+/**
+ * Created by Ng Chan-fung on 2018/4/14.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
