f1a36dc4b35b256393dd5d2a2533b35d48b6ab09
ngchanfung <ngchanfung@gmail.com>
2018-04-15 23:47:30 +0800

    print dev5

--- src/com/camel/hellogit/HelloGit.java
+++ src/com/camel/hellogit/HelloGit.java
@@ -10,5 +10,6 @@ public class HelloGit {
         System.out.println("dev2");
         System.out.println("dev3");
         System.out.println("dev4.");
+        System.out.println("dev5");
     }
 }
